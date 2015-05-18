deb1ccc408f915e88a800c9ec0d265b52386d61b
urbanma <urban.martin7@gmail.com>
2015-05-18 17:27:17 +0200

    test

---
+++ mois/hibernate.cfg.xml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace mois
+{
+    public partial class hibernate_cfg : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
